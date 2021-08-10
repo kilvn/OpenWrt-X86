87892ee103c345ffed9928145a23023d4c030018
xnxy2012 <35721438+xnxy2012@users.noreply.github.com>
2021-08-10 20:30:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,4 +17,4 @@
 rm -rf luci-theme-argon_new package/feeds/kenzo
 rm -rf luci-app-passwall package/feeds/kenzo
 rm -rf luci-app-vssr package/feeds//kenzo
-rm -rf luci-theme-argon package/feeeds/lean
+rm -rf luci-theme-argon package/lean
