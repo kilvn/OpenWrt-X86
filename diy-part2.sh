6ebe59defada0b3d1bcf73a6b44f3874e6e37ebc
xnxy2012 <35721438+xnxy2012@users.noreply.github.com>
2021-08-10 19:36:07 +0800

    update

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,6 +14,6 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 #移除无用软件包
-rm -rf luci-theme-argon_new package/openwrt-packages
-rm -rf luci-app-passwall package/openwrt-packages
+rm -rf luci-theme-argon_new package/kenzo
+rm -rf luci-app-passwall package/kenzo
 rm -rf luci-theme-argon package/lean
