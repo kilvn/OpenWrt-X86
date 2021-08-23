acb978d07d9ffe6761c7866765a9a1d2a370713a
xnxy2012 <35721438+xnxy2012@users.noreply.github.com>
2021-08-23 12:10:30 +0800

    Update diy-part2.sh

--- K3/diy-part2.sh
+++ K3/diy-part2.sh
@@ -15,3 +15,6 @@
 
 #download apk       
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/lwz322/k3screenctrl_build.git package/k3screenctrl_build
+git clone https://github.com/lwz322/k3screenctrl.git package/k3screenctrl
+git clone https://github.com/lwz322/luci-app-k3screenctrl.git package/luci-app-k3screenctrl
