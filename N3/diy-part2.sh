3c6240704c93cd14422a12fd1b5310847c3881ce
xnxy2012 <35721438+xnxy2012@users.noreply.github.com>
2023-08-11 15:52:58 +0800

    Update diy-part2.sh

--- N3/diy-part2.sh
+++ N3/diy-part2.sh
@@ -13,6 +13,11 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
+echo '定制主机名'
+sed -i "s/hostname='OpenWrt'/hostname='Newifi-D2'/g" package/base-files/files/bin/config_generate
+cat package/base-files/files/bin/config_generate |grep hostname=
+echo '=========Rewrite hostname done!========='
+
 #download apk       
 git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/lucipc
 git clone https://github.com/xiaorouji/openwrt-passwall2.git package/lucipb
